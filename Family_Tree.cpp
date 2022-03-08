cfb58751e318f8b7381e3ba10a921405d6afe759
EliasWR <eliaswoierefsdal@gmail.com>
2022-03-08 10:14:52 +0100

    Initial commit

---
+++ Family_Tree.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Elias Woie Refsdal on 07/03/2022.
+//
+
+#include <iostream>
+int main (){
+        std::cout << "Hello";
+        return 0;
+}
---
+++ Person.h
@@ -0,0 +1,8 @@
+//
+// Created by Elias Woie Refsdal on 08/03/2022.
+//
+
+#ifndef FAMILYTREE_PERSON_H
+#define FAMILYTREE_PERSON_H
+
+#endif //FAMILYTREE_PERSON_H
