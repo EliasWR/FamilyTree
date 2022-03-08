75bde2c4039886b5563a6187a2cdbd98ff61c6db
EliasWR <eliaswoierefsdal@gmail.com>
2022-03-08 14:25:29 +0100

    Overriding test

--- Family_Tree.cpp
+++ Family_Tree.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 int main () {
     std::cout << "Hello Ginger Slayer Man Elias"<<std::endl;
-    std::cout<<"Hello i am hot and sexy";
+    std::cout<<"Hello i am hot and sexy and cool";
     return 0;
 }
 
