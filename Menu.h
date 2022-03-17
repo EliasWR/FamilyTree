731125e5d18c661e5cf1ab46c019185fccf389fd
EliasWR <eliaswoierefsdal@gmail.com>
2022-03-17 22:11:55 +0100

    Implemented some minor changes

--- Menu.h
+++ Menu.h
@@ -5,7 +5,25 @@
 #ifndef FAMILYTREE_MENU_H
 #define FAMILYTREE_MENU_H
 
+#include <iostream>
+
 class Menu {
+private:
+    int _state = 0;
+public:
+    void setState(int state){
+        _state = state;
+    }
+    int getState() const {
+        return _state;
+    }
+    void greeting (){
+        std::cout << "                  " << "Welcome to 'FamilyTree'!"<< std::endl;
+        std::cout << "In this program you can make and modify a family tree of your own" << std::endl;
+        std::cout << "To navigate this program you type the number of your desired command followed by enter." << std::endl;
+    }
+
+
 
 };
 
