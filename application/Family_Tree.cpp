a82f77c5a8dce3d03dc030ee93abe96459bfb387
EliasWR <eliaswoierefsdal@gmail.com>
2022-05-19 14:27:43 +0200

    neat main

--- application/Family_Tree.cpp
+++ application/Family_Tree.cpp
@@ -1,48 +1,11 @@
 #include "Menu.hpp"
-#include "Node.hpp"
 #include "Person.hpp"
-#include <iostream>
 
 int main() {
-  /*
-  auto a = std::make_shared<Node<Person>>(Person("Gunnar", "Sonsteby"));
-  a->add (Person("Erik", "Sonsteby"));
-  a->add (Person("Anita", "Sonsteby"));
-  a->add (Person("Hans", "Sonsteby"));
-  a->add (Person("Lillian", "Sonsteby"));
-
-  Person p("Henrik", "Sonsteby");
-  auto lambda1 = [p](Node<Person> &node) {node.add(p);};
-  a->traverseDepth (lambda1,"Erik","Sonsteby");
-
-  nlohmann::json j;
-  std::string fileName = "FamilyTree.json";
-  JsonFile json(a, fileName);
-
-  json.openFile();
-
-  j = json.writePerson();
-  json.jsonToFile(j);
-
-  std::string s = j.dump(4);
-  std::cout << s << std::endl;
-
-  json.closeFile ();
-
-  j = json.readFile(j);
-  auto emptyNode = std::make_shared<Node<Person>> ();
-  auto b = json.nodeFromJson(j, emptyNode);
-
   Menu<Person> m;
-  m.printTree(b);
-*/
-
-  Menu<Person> m;
-
   m.greeting();
   while (m.getState() != 9) {
     m.mainScreen();
   }
-
   return 0;
 }
